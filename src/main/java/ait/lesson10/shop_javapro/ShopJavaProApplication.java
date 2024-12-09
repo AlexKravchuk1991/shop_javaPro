de4db3f46c369cd73ce8acb22ba6e7dce9e6b288
AlexKravchuk1991 <alex_krava@ukr.net>
2024-12-09 22:26:01 +0100

    first commit

---
+++ src/main/java/ait/lesson10/shop_javapro/ShopJavaProApplication.java
@@ -0,0 +1,13 @@
+package ait.lesson10.shop_javapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopJavaProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopJavaProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ait/lesson10/shop_javapro/ShopJavaProApplicationTests.java
@@ -0,0 +1,13 @@
+package ait.lesson10.shop_javapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopJavaProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
